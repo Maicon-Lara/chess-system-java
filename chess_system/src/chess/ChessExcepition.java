06a6e52e6890cccd211d174d941fa550372115b3
Maicon Lara <m.lara93@outlook.com>
2024-09-09 13:44:24 -0300

    Chess layer and printing the board

---
+++ chess_system/src/chess/ChessExcepition.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessExcepition extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExcepition(String msg) {
+		super(msg);
+	}
+
+}
